9cce2e546b97c381c1b6c3c3fc02930949a8f8c1
Nelson <nelsonalejandroramosrivera@gmail.com>
2022-02-05 15:37:05 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://porth.app/logo_white.svg" />
+    <Message msg="Welcome to Your Vue.js App" />
+    <!--  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+import Message from "./components/message.vue";
+
+export default {
+  name: "App",
+  components: {
+    /*  HelloWorld, */
+    Message,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  margin-top: -100px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  background: linear-gradient(250deg, #3745a0, #212a64);
+  z-index: 1;
+  position: relative;
+  padding-top: 7em;
+  padding-bottom: 9em;
+}
+</style>
---
+++ src/components/message.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="content">
+    <textarea class="text" v-model="text" name="comentarios" rows="10" cols="40">
+Escribe aquí tu mensaje</textarea
+    >
+    <br />
+    <button v-on:click="enviar(text)">Enviar</button>
+    <div class="mensajes">
+      <div class="data" v-for="data in result" :key="data.id">
+        {{ data.message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+    text:null,
+  }),
+  async created() {
+    let response = await axios.post(
+      "http://ec2-3-129-245-136.us-east-2.compute.amazonaws.com:3000/write"
+    );
+    this.result = response.data;
+  },
+  methods:{
+    async enviar(data) {
+    if(data !=null){
+      let response = await axios.post(
+        "http://ec2-3-129-245-136.us-east-2.compute.amazonaws.com:3000/write", {"message":data}
+      );
+      this.result = response.data;
+    }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.mensajes{
+  margin-top: 50px;
+}
+textarea.text {
+  margin-top: 50px;
+}
+</style>
